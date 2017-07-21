26229d3b9b130b46971c0175b9379cfc4b845ef3
Ahmed Fasih <wuzzyview@gmail.com>
2017-07-21 11:56:02 -0400

    With a low number of revs_limit, disk usage is small
    With 2 revs_limit (instead of 1000, the default), the SQLite file for
    Pouch is ~2MB. But I thought Couch stored just revs of old versions, and
    not a full copy? Does Couch deviate from this?

--- diskPouchDb.ts
+++ diskPouchDb.ts
@@ -3,5 +3,5 @@ PouchDB.plugin(require('pouchdb-adapter-node-websql'));
 PouchDB.plugin(require('pouchdb-upsert'));
 
 export type Db = PouchDB.Database<{}>;
-export let db: Db = new PouchDB('mypouchdb.db', { adapter: 'websql' });
+export let db: Db = new PouchDB('mypouchdb.db', { adapter: 'websql' /*, revs_limit: 2 */ });
 // export let db: Db = new PouchDB('./mypouchlevel');
