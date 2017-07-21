4dcfe7030e1bc3c25fba86e567f99f550b4b22cf
Ahmed Fasih <wuzzyview@gmail.com>
2017-07-21 10:57:05 -0400

    Remove level packages

---
+++ levelToPouch.ts
@@ -0,0 +1,13 @@
+var fs = require('fs');
+var lines = fs.readFileSync("mydb.txt", "utf8").trim().split('\n');
+var kv = lines.map(s=>s.split('→')).map(([key,json])=>[key, JSON.parse(json)])
+
+import {submit} from "./storageServer";
+import { db, Db } from "./diskPouchDb";
+async function port(kvs) {
+    for (let kv of kvs) {
+        let [key, val] = kv;
+        await submit(db, val.user, val.docId, val.factId, val.ebisuObject, val.updateObject, val.createdAt);
+    }
+}
+port(kv);
