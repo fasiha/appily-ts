b2b837d0d40b8463d48f0db0a319bc4e0b5bb4c9
Ahmed Fasih <wuzzyview@gmail.com>
2017-07-21 12:33:52 -0400

    Webserver imports pouchdb and serves it
    Try with:
    $ curl localhost:3001/db/mypouchlevel/_all_docs

--- diskPouchDb.ts
+++ diskPouchDb.ts
@@ -1,7 +1,9 @@
 import PouchDB = require('pouchdb');
-PouchDB.plugin(require('pouchdb-adapter-node-websql'));
+// PouchDB.plugin(require('pouchdb-adapter-node-websql'));
 PouchDB.plugin(require('pouchdb-upsert'));
 
 export type Db = PouchDB.Database<{}>;
-export let db: Db = new PouchDB('mypouchdb.db', { adapter: 'websql' /*, revs_limit: 2 */ });
-// export let db: Db = new PouchDB('./mypouchlevel');
+// export let db: Db = new PouchDB('mypouchdb.db', { adapter: 'websql' /*, revs_limit: 2 */ });
+export let db: Db = new PouchDB('mypouchlevel');
+
+export const pouchdbImport = PouchDB;
--- webServer.ts
+++ webServer.ts
@@ -1,15 +1,9 @@
-import levelup = require('levelup');
-import { makeShoeInit } from "./storageServer";
-import { db } from "./diskDb";
-
+import { db, pouchdbImport } from "./diskPouchDb";
 
 const express = require('express');
-const shoe = require('shoe');
 
 const port = 3001;
 const app = express();
 app.use('/', express.static('client'));
-
-
-const sock = shoe(makeShoeInit(db));
-sock.install(app.listen(port, () => { console.log(`Started: http://127.0.0.1:${port}`) }), '/api/ml');
+app.use('/db', require('express-pouchdb')(pouchdbImport, { mode: 'fullCouchDB' }));
+app.listen(port, () => console.log('Running!'));
